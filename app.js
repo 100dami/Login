d1969a8c9149b49cc05b007f0a626c504fd33eab
백다미 <s20033@gsm.hs.kr>
2021-03-21 02:05:35 +0900

    Update Files

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT ||3000;
+const mongoose = require("mongoose");
+let url = "mongodb://localhost:27017//dalhav";
+// DB연결
+mongoose.connect(url, {useNewUrlParser:true});
 
 /*app.get("/", function(req,res) {
     res.send("Hello World");
@@ -14,7 +18,7 @@ app.listen(PORT, function (){
     console.log("Example app listening on port", PORT);
 });*/
 
-// routes (라우터?)
+// routes (라우터)
 const indexRoute = require("./routes/index");
 
 // 뷰엔진 설정
@@ -22,8 +26,9 @@ app.set("views",__dirname+"/views");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public")); // 절대경로
 
-// use routes (라우터의 사용)?
+// use routes (라우터의 사용)
 app.use("/", indexRoute);
+
 
 // listen
 app.listen(PORT,function(){
